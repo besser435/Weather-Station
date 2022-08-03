6e469120e2a40c8a83899dc3c90239a1f76daa81
Brandon <68145204+besser435@users.noreply.github.com>
2022-08-03 03:36:17 -0700

    v1.0

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+from random import randint
 import time
 import busio
 import digitalio
@@ -6,9 +7,9 @@ import board
 import neopixel
 import adafruit_rfm69
 from adafruit_ms8607 import MS8607
-import traceback
 
-version = "Weather Station v0.1 (Station)"
+
+version = "Weather Station v1.0 (Station)"
 print(version)
 
 # I2C
@@ -16,7 +17,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Neopixel
 led_neo = neopixel.NeoPixel(board.NEOPIXEL, 1)
-led_neo.brightness = 0.02
+led_neo.brightness = 0.04
 
 # Pressure, Humidity, Temp
 weath_sens = MS8607(i2c)
@@ -30,24 +31,20 @@ prev_packet = None
 # Optionally set an encryption key (16 byte AES key). MUST match both
 # on the transmitter and receiver (or be set to None to disable/the default).
 #rfm69.encryption_key = b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
-
-
-# test that encryption is working
-# clean things up into functions
-# add acknowledgements: 
 # https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/advanced-circuitpython-library-rfm9x-rfm69-tweaking-parameters
 # https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/advanced-circuitpython-rfm69-library-usage
 
 
-led_neo[0] = (0, 255, 0)
 def main():
+    cycle = 0
     while True:
         led_neo[0] = (0, 0, 255)
 
         weather_data= ( 
         str(weath_sens.temperature) + "," + 
         str(weath_sens.pressure) + "," + 
-        str(weath_sens.relative_humidity)
+        str(weath_sens.relative_humidity) + "," + 
+        str(cycle)
         )
 
         send_data = bytes(weather_data, "\r\n","utf-8")
@@ -55,127 +52,10 @@ def main():
         print(weather_data)
         print("Sent weather data")
         led_neo[0] = (0, 0, 0)
+        cycle += 1
         time.sleep(1)
-        
 
-"""def main():
-    while True:
-        packet = None
-        packet = rfm69.receive()
-        # If no packet was received during the timeout then None is returned.
-        if packet is None:
-            # Packet has not been received
-            print("Received nothing! Listening again...")
-        else:
-            led_neo[0] = (0, 0, 255)
-            # raw packet
-            print()
-            print("Received (raw bytes): {0}".format(packet))
-
-            # processed packet
-            packet_text = str(packet, "ascii")
-            print("Received (ASCII): {0}".format(packet_text))
-
-            if packet_text == "request_weather\r\n":
-                print("Weather request received")
-
-                # do these need to be casted?
-                weather_data= ( 
-                str(weath_sens.temperature) + "," + 
-                str(weath_sens.pressure) + "," + 
-                str(weath_sens.relative_humidity)
-                )
-
-
-            
-                print(weather_data)
-                send_data = bytes(weather_data, "\r\n","utf-8")
-                rfm69.send(send_data)
-                print("Sent weather data")
-                led_neo[0] = (0, 255, 0)
-
-"""
-
-
-try:  # sometimes the radios get fucky wucky
-    print("Running main()")
-    print("RSSI: " + str(rfm69.rssi))
-    print("Power: " + str(rfm69.tx_power))
-    main()
-except UnicodeError:    # maybe change to general traceback
-    print("Unicode error")
-    led_neo[0] = (255, 0, 0)
-    #send a message to the server that the request failed has failed
-    main()
-
-
-
-"""
-
-# SPDX-FileCopyrightText: 2018 Tony DiCola for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple example to send a message and then wait indefinitely for messages
-# to be received.  This uses the default RadioHead compatible GFSK_Rb250_Fd250
-# modulation and packet format for the radio.
-import board
-import busio
-import digitalio
-import time
-import adafruit_rfm69
-
-
-# Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
-
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
-CS = digitalio.DigitalInOut(board.D5)
-RESET = digitalio.DigitalInOut(board.D6)
-# Or uncomment and instead use these if using a Feather M0 RFM69 board
-# and the appropriate CircuitPython build:
-# CS = digitalio.DigitalInOut(board.RFM69_CS)
-# RESET = digitalio.DigitalInOut(board.RFM69_RST)
-
-# Define the onboard LED
-LED = digitalio.DigitalInOut(board.D13)
-LED.direction = digitalio.Direction.OUTPUT
-
-# Initialize SPI bus.
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# Initialze RFM radio
-rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ)
-
-# Optionally set an encryption key (16 byte AES key). MUST match both
-# on the transmitter and receiver (or be set to None to disable/the default).
-rfm69.encryption_key = (
-    b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
-)
-
-# Print out some chip state:
-print("Temperature: {0}C".format(rfm69.temperature))
-print("Frequency: {0}mhz".format(rfm69.frequency_mhz))
-print("Bit rate: {0}kbit/s".format(rfm69.bitrate / 1000))
-print("Frequency deviation: {0}hz".format(rfm69.frequency_deviation))
-
-# Send a packet.  Note you can only send a packet up to 60 bytes in length.
-# This is a limitation of the radio packet size, so if you need to send larger
-# amounts of data you will need to break it into smaller send calls.  Each send
-# call will wait for the previous one to finish before continuing.
-
-# Wait to receive packets.  Note that this library can't receive data at a fast
-# rate, in fact it can only receive and process one 60 byte packet at a time.
-# This means you should only use this for low bandwidth scenarios, like sending
-# and receiving a single message at a time.
-print("Waiting for packets...")
-yes = 0
-while True:
-    rfm69.send(bytes("hi mom\r\n", "utf-8"))
-    #rfm69.send("Hello world! from feather")
-    print("Sent yesnt")
-    yes += 1
-    print(yes)
-
-   
-"""
+print("Running main()")
+print("RSSI: " + str(rfm69.rssi))
+print("Power: " + str(rfm69.tx_power))
+main()
--- ws_host.py
+++ ws_host.py
@@ -1,39 +1,44 @@
 """
-Architecture:
-Pi requests data every 20 minutes of the hour
-Station sends data
-Pi logs data to .csv file and uploads it to a github repo
-
 Things to log:
 Temp, pressure, humidity, time, weather data from weather api on Pi, errors
 
 
 Host Features:
-Uploads database to github repo
+Uploads database to its own github repo
 Displays last readings
 if no data in an hour, displays an error
 button to refresh (not log) new data
+
+Last answer
+https://forums.raspberrypi.com/viewtopic.php?t=304628
+
+
+to do:
+Add weather API data
+Add error logging for station and server
+Clean up csv structure
+
+
 """
 
 
-version = "Weather Station v0.1 (Server)"
+version = "Weather Station v1.0 (Server)"
 print(version)
 
-
+from colorama import init  
+init()
+from colorama import Fore, Back, Style
+init(autoreset=True)
 import time
 import datetime
-
+import traceback
 from requests import request
 import busio
 from digitalio import DigitalInOut, Direction, Pull
 import board
-import threading
-import displayio
-#import adafruit_displayio_ssd1306 # NEW LIBRARY
-import adafruit_ssd1306 # OLED display, this libary is deprecated. update to displayio
-
-
+import adafruit_ssd1306 # OLED display, this library is deprecated. update to displayio
 import adafruit_rfm69
+
 
 # Button A
 btnA = DigitalInOut(board.D5)
@@ -52,20 +57,10 @@ btnC.pull = Pull.UP
 
 # I2C
 i2c = busio.I2C(board.SCL, board.SDA)
-#i2c = board.I2C() 
 
 # 128x32 OLED Display
 reset_pin = DigitalInOut(board.D4)
-
-
-"""display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
-#display_bus = displayio.I2CDisplay(i2c)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
-"""
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin) # OLD
-
-
-# Clear the display.
 display.fill(0)
 display.show()
 width = display.width
@@ -82,9 +77,6 @@ prev_packet = None
 #rfm69.encryption_key = b"\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08"
 
 
-
-
-
 # options
 FILE_NAME = "weather_db.csv" 
 file_location = "/home/pi/Desktop/" 
@@ -95,102 +87,145 @@ weather_api_key = "1392d31baeec1ab9f5d2bd99d5ec04aa"
 
 # storage
 last_update = None
+got_update_this_min = False # so that we don't log data multiple times when it is time to log
+current_display_page = 0
+
 
 with open(file_location + FILE_NAME, "a") as f: 
-    f.write("temp,pressure,humidity,time_date\n") # ADD: api_temp, api_pressure, api_humidity, api_conditions, api_time
+    f.write("temp,pressure,humidity,transmission_no,time_date\n") # ADD: api_temp, api_pressure, api_humidity, api_conditions, api_time
+
+display.fill(0)
+display.text("Starting", 28, 13, 1)
+display.show() 
+
+# this stuff is so errors dont happen when there is no data.
+# for example when the program first starts
+temp = None
+pressure = None
+humidity = None
+trans_no = None
+
 
 try:
-    while True:
-        #time.sleep(0.5)
-        get_update = bytes("request_weather\r\n", "utf-8")
+    def display_info(page):
+        #get_update(0) #probably best to leave this out here, so we have more control. we dont want to
+        # get_update(1), display_info(), get_update(0). that might cause bugs, idk
+        dt_time = datetime.datetime.now()
+        time_date = dt_time.strftime("%m-%d-%Y, %H:%M:%S") 
         
-        weekday = str(datetime.datetime.today().weekday())
-        current_time = str(datetime.datetime.now().strftime("%I:%M %p"))
-
-        display.fill(0)
-        display.text(weekday, 0, 0, 1)
-        display.text(current_time, 0, 25, 1)
-        display.show()
-        
-        
-        # log the data every 20 minutes (Only does it once per hour rn)
-        # fetch an update from every 20 minutes
-
-
-        #if datetime.datetime.now().minute == 14: # test for exact minute
-        #if datetime.datetime.now().minute % 2 == 0:
-        if True: # for testing
+        if page == 0:
             display.fill(0)
-            rfm69.send(get_update)
-            display.text("Requested update", 0, 0, 1)
-            display.show()    
-            print("Requested update")
-            #time.sleep(0.05) #needed? longer? shorter?
-
-        # manually fetch an update
-        elif not btnA.value:
-            display.fill(0)
-            rfm69.send(get_update)
-            display.text("Requested update manually", 0, 25, 1)
+            display.text("Temp, update, time:", 0, 0, 1)
+            display.text(str(temp) + "c", 0, 13, 1)
+            # the last time data was added to the DB and the current time
+            display.text(
+            "DBU: " + str(last_update) + 
+            " T: " + str(dt_time.strftime("%H")) + ":" + str(dt_time.strftime("%M")), 0, 25, 1
+            )
             display.show()
-            print("Requested update manually")
 
+        if page == 1:
+            display.fill(0)
+            display.text(("%.1f" % float(temp)) + "c    " + "%.1f" % ((float(temp) * 1.8) + 32) + "f", 0, 0, 1) # HOLY FUCK
+            display.text("%.1f" % float(pressure) + "mbar", 0, 13, 1)
+            display.text("%.1f" % float(humidity) + "%    #: " + str(trans_no), 0, 25, 1)
+            display.show() 
 
-        # the code below should be in a function that the two conditions above call
-        # fetch the data from the returned packet
+        if page == 2:
+            display.fill(0)
+            display.text("Stored Data", 27, 13, 1)
+            display.show()
+            time.sleep(2)
+            display_info(current_display_page)    
+    
+
+    def get_update(store):
+        # store is to determine if it will store the data to the DB,
+        # or just fetch an update to display numbers and not store it
+        global temp, pressure, humidity, time_date, dt_time, trans_no, last_update
+
         packet = None
-        packet = rfm69.receive(keep_listening=True, timeout=1.0)
+        packet = rfm69.receive(keep_listening=True)
+        #packet = rfm69.receive(keep_listening=True, timeout=1.0)
         if packet is None:
             print("Received nothing! Listening again...")
-            display.fill(0)
-            display.text("Received nothing", 0, 0, 1)
-            display.show()
-        else:
-            prev_packet = packet
-            packet_text = str(packet, "ascii")
-            print("Received (raw bytes): " + str(packet))
-            print("Received (formatted): " + packet_text)
-
-            display.fill(0)
-            display.text("Got data", 0, 0, 1)
-            display.show()
             
+        else:
+            #prev_packet = packet
             # returned data
+            packet_text = str(packet, "ascii")
+            #print("Received (formatted): " + packet_text)
             temp = packet_text.split(",")[0]
             pressure = packet_text.split(",")[1]
             humidity = packet_text.split(",")[2]
-            time_date = datetime.datetime.now() # the station has no time, so I have to rely on the Pi's time
-            last_update = time_date
-
+            trans_no = packet_text.split(",")[3]
+            dt_time = datetime.datetime.now()
+            time_date = dt_time.strftime("%m-%d-%Y, %H:%M:%S") 
             print("Temp: " + temp)
             print("Pressure: " + pressure)
             print("Humidity: " + humidity)
             print("Time: " + str(time_date))
+            print("Transmission #: " + trans_no)
             print()
-            #time.sleep(1)
 
-        
+            if store == 1:
+                print(Fore.GREEN + "Storing data to DB...")
+                # log the data to the file
+                with open(file_location + FILE_NAME, "a") as f: 
+                    f.write(str(temp) + "," + str(pressure) + "," + str(humidity)
+                    + "," + str(trans_no) + "," + str(time_date) + "\n")
+
+                last_update = str(dt_time.strftime("%H")) + ":" + str(dt_time.strftime("%M"))
+                display_info(2)
+                #display.fill(0)
+                #display.text("Got update", 0, 0, 1)
+                #display.show()
+                #time.sleep(1)
+
+            else:
+                print(Fore.RED + "Not storing data to DB")
+    get_update(0)
+    display_info(0)
+
+    def main():
+        get_update(1)
+        global current_display_page, got_update_this_min
+        while True:
+            get_update(0)
+            display_info(current_display_page)
+            if datetime.datetime.now().minute % 10 == 0 and got_update_this_min == False:
+                #if True: # for testing
+                print("Auto refresh")
+                get_update(1)
+                display_info(0)
+                time.sleep(1)
+                got_update_this_min = True  # prevents multiple updates per minute, only want one
+                print(Fore.YELLOW + str("got_update_this_min time condition " + str(got_update_this_min)))
+
+            if datetime.datetime.now().minute % 10 != 0:    # reset the var so its ready for next time
+                got_update_this_min = False
+                
+            if not btnA.value: 
+                current_display_page = 0
+                get_update(0) 
+                display_info(0)
+                
+            if not btnB.value:
+                current_display_page = 1
+                get_update(0) 
+                display_info(1)
+
+            if not btnC.value:
+                get_update(1)     
+    main()
 
 
-            # log the data to the file
-            with open(file_location + FILE_NAME, "a") as f: 
-                f.write(str(temp) + "," + str(pressure) + "," + str(humidity) + "," + str(time_date) + "\n")
-
-
-        # error checking
-        """if time_date - last_update > datetime.timedelta(hours=1):
-            display.text("Error: No data in last hour", 25, 15, 1)
-            with open(file_location + FILE_NAME, "a") as f: 
-                f.write("Stale data. last update: " + str(last_update) + "current time: " + time_date + "\n")
-
-            print("Error: No data in last hour")
-            time.sleep(0.05)
-        """
-        display.show()
-        #time.sleep(0.1)
-except IndexError:
-    # sometimes the radio receives its own request for some reason
-    print("F-fucky wucky detected! IndexError bwyte awway out of wange!") # war crime soup
-
-
- 
+except Exception:
+    # sometimes the radio receives its own request for some reason. 
+    # It will also throw temp and display errors if there is no connection to the station
+    print(Fore.RED + "f-fucky wucky detected! Getting any telwemitry fwom the station?") # war crime soup
+    print(traceback.format_exc())
+    display.fill(0)
+    display.text("Error, stopping.", 0, 0, 1)
+    display.show() 
+    
